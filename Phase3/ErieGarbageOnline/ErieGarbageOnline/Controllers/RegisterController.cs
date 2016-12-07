d4ef9df90bbdf0265ded0499d8a979fb5ccdfa9a
jakewheeler <jakezro00@aim.com>
2016-12-07 17:46:00 -0500

    added input validation that changes text box colors if input is incorrect

--- Phase3/ErieGarbageOnline/ErieGarbageOnline/Controllers/RegisterController.cs
+++ Phase3/ErieGarbageOnline/ErieGarbageOnline/Controllers/RegisterController.cs
@@ -1,8 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using System.Windows.Controls;
+using System.Windows.Media;
 using ErieGarbageOnline.Database;
 using ErieGarbageOnline.Models;
 using ErieGarbageOnline.Views.Login;
@@ -11,7 +11,7 @@ namespace ErieGarbageOnline.Controllers
 {
     class RegisterController
     {
-        private Register _view;
+        private readonly Register _view;
         private readonly EGODatabase _database;
 
         public RegisterController(Register register)
@@ -20,17 +20,98 @@ namespace ErieGarbageOnline.Controllers
             _database = EGODatabase.Create();
         }
 
-        public bool RegisterCustomer(Customer newCust)
+        /// <summary>
+        /// Validates text fields before customer object is even created
+        /// </summary>
+        /// <returns> Boolean specifying whether the input is acceptable </returns>
+        private bool InputValidation()
         {
-            return CustomerIsValid(newCust) && _database.AddCustomer(newCust);
+            bool isValid = true;
+
+            SetAllBoxesToWhite();
+
+            if (!_view.EmailBox.Text.Contains("@"))
+            {
+                _view.EmailBox.Background = Brushes.Gold;
+                isValid = false;
+            }
+
+            if (!Regex.IsMatch(_view.FirstNameBox.Text, @"^[^0-9]+$"))
+            {
+                _view.FirstNameBox.Background = Brushes.Gold;
+                isValid = false;
+            }
+
+            if (!Regex.IsMatch(_view.LastNameBox.Text, @"^[^0-9]+$"))
+            {
+                _view.LastNameBox.Background = Brushes.Gold;
+                isValid = false;
+            }
+
+            if (!Regex.IsMatch(_view.StateBox.Text, @"^[^0-9]+$"))
+            {
+                _view.StateBox.Background = Brushes.Gold;
+                isValid = false;
+            }
+
+            if (!Regex.IsMatch(_view.CountryBox.Text, @"^[^0-9]+$"))
+            {
+                _view.CountryBox.Background = Brushes.Gold;
+                isValid = false;
+            }
+
+            foreach (var textBox in GetAllTextBoxes().Where(textBox => string.IsNullOrEmpty(textBox.Text)))
+            {
+                textBox.Background = Brushes.Gold;
+                isValid = false;
+            }
+
+            return isValid;
         }
 
+        /// <summary>
+        /// Collects all text boxes on the view and stores them in a list
+        /// </summary>
+        /// <returns> List of text boxes found on the view </returns>
+        private IEnumerable<TextBox> GetAllTextBoxes()
+        {
+            var collection = _view.RegisterGrid.Children.OfType<TextBox>();
+
+            return collection.ToList();
+        }
+
+        /// <summary>
+        /// Inits text boxes on view to white
+        /// </summary>
+        private void SetAllBoxesToWhite()
+        {
+            foreach (var box in GetAllTextBoxes())
+            {
+                box.Background = Brushes.White;
+            }
+        }
+
+        /// <summary>
+        /// Adds a customer to the database
+        /// </summary>
+        /// <param name="newCust"></param>
+        /// <returns> Boolean specifying whether action was accomplished or not </returns>
+        public bool RegisterCustomer(Customer newCust)
+        {
+            return InputValidation() && CustomerIsValid(newCust) && _database.AddCustomer(newCust);
+        }
+
+        /// <summary>
+        /// Checks the validity of the customer object
+        /// </summary>
+        /// <param name="cust"> Customer object created from inputs </param>
+        /// <returns> Boolean specifying whether the customer info is valid or not </returns>
         private bool CustomerIsValid(Customer cust)
         {
             if (cust == null)
                 return false;
 
-            if (cust.CheckValidity() == false)
+            if (!cust.CheckValidity())
                 return false;
 
             if (string.IsNullOrEmpty(cust.Firstname))
